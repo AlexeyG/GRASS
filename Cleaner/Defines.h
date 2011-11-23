27922b218bc6f24d7aeb93885da73151a1af51f4
grizenko.a <grizenko.a@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-11-23 16:55:01 +0000

    - Changing directory structure (to match actual unix names)

--- Cleaner/Defines.h
+++
@@ -1,8 +0,0 @@
-// Application version
-#define VERSION "0.001"
-
-// Application developer
-#define AUTHOR "Alexey Gritsenko"
-
-// Development date
-#define DATE "09.11.2011"
--- Linker/Defines.h
+++
@@ -1,8 +0,0 @@
-// Application version
-#define VERSION "0.001"
-
-// Application developer
-#define AUTHOR "Alexey Gritsenko"
-
-// Development date
-#define DATE "29.03.2011"
