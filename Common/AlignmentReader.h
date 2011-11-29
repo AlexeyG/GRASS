786c6ad9e46ea8c8a45697994200798ee485d0d7
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-11-29 14:09:22 +0000

    - Updated header

--- Common/AlignmentReader.h
+++ Common/AlignmentReader.h
@@ -19,8 +19,10 @@ public:
 	bool IsOpen() const;
 	bool GetNextAlignmentGroup(vector<BamAlignment> &alg);
 	bool GetNextAlignmentGroup(BamAlignment &alignment, vector<XATag> &tags);
-        const RefVector & GetReferences() const;
-        int GetReferenceCount() const;
+        
+public:
+    const RefVector & GetReferences() const;
+    int GetReferenceCount() const;
 
 private:
 	void parseXATag(const string &str, vector<XATag> &tags);
