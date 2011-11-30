5ab2e4720a82967158a9262b193923f073156421
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-11-30 09:05:02 +0000

    - Added ReadCoverageRepeatDetecter class for repeat detection using Velvet algoithm (page 69 of Zerbino PhD Thesis)

---
+++ Common/ReadCoverageRepeatDetecter.cpp
@@ -0,0 +1,35 @@
+#include "ReadCoverageRepeatDetecter.h"
+#include "DataStore.h"
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+vector<int> ReadCoverageRepeatDetecter::Detect(double expectedCoverage, const ReadCoverage &coverage, const DataStore &store, double uniqunessCutoff)
+{
+    double expectedStarts = expectedCoverage / coverage.AverageReadLength;
+    vector<int> repeatContigs;
+    int nContigs = coverage.ReadLocations.size();
+    for (int i = 0; i < nContigs; i++)
+    {
+        double observedMean = 0.0;
+        int observedGroups = 0;
+        int contigLength = store[i].Sequence.Nucleotides.length();
+        vector<int> readPositions(coverage.ReadLocations[i]);
+        sort(readPositions.begin(), readPositions.end());
+        vector<int>::const_iterator pos = readPositions.begin();
+        while (pos != readPositions.end())
+        {
+            observedGroups++;
+            int count = 1;
+            vector<int>::const_iterator start = pos++;
+            while (pos != readPositions.end() && *pos == *start)
+                count++, pos++;
+            observedMean += count;
+        }
+        observedMean /= (double)observedGroups;
+        double logRatio = log(2.0) / 2.0 + contigLength * (expectedStarts * expectedStarts - observedMean * observedMean / 2.0) / (2.0 * expectedStarts);
+        if (logRatio < uniqunessCutoff)
+            repeatContigs.push_back(i);
+    }
+}
---
+++ Common/ReadCoverageRepeatDetecter.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ReadCoverageRepeatDetecter.h
+ * Author: alexeyg
+ *
+ * Created on November 30, 2011, 9:28 AM
+ */
+
+#ifndef _READCOVERAGEREPEATDETECTER_H
+#define	_READCOVERAGEREPEATDETECTER_H
+
+#include <vector>
+#include "ReadCoverage.h"
+
+using namespace std;
+
+class ReadCoverageRepeatDetecter
+{   
+public:
+    static vector<int> Detect(double expectedCoverage, const ReadCoverage &coverage, const DataStore &store, double uniqunessCutoff);
+};
+
+#endif	/* _READCOVERAGEREPEATDETECTER_H */
