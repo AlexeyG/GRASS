6d968de7145f3a8e21e50be473cca222b0cc34cd
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-04 12:10:04 +0000

    - Segfault

--- coverageUtil/coverage.cpp
+++ coverageUtil/coverage.cpp
@@ -58,7 +58,7 @@ bool calculateDepth(const ReadCoverage &coverage, const Sequences &contigs, Dept
         for (vector<int>::const_iterator it = coverage.ReadLocations[i].begin(); it != coverage.ReadLocations[i].end(); it++)
         {
             for (int k = *it; k < *it + avgReadLength && k < contigLength; k++)
-                t++;
+                t[k]++;
                 //depth[i][k]++;
         }
     }
