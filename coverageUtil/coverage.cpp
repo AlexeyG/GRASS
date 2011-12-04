f5f50a339fcbedb7430a99b630d60d5ccccf6c93
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-04 11:47:19 +0000

    - Segfault

--- coverageUtil/coverage.cpp
+++ coverageUtil/coverage.cpp
@@ -52,7 +52,7 @@ bool calculateDepth(const ReadCoverage &coverage, const Sequences &contigs, Dept
     {
         int contigLength = contigs[i].Nucleotides.length();
         //depth[i].assign(contigLength, 0);
-        int *t = new int[contigLength];
+        int t[] = new int[contigLength]; 
         for (vector<int>::const_iterator it = coverage.ReadLocations[i].begin(); it != coverage.ReadLocations[i].end(); it++)
         {
             for (int k = *it; k < *it + avgReadLength && k < contigLength; k++)
