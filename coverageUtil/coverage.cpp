a530e4514585e8b3396404127d23ea9c3b39cb89
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-04 13:01:03 +0000

    - Segfault

--- coverageUtil/coverage.cpp
+++ coverageUtil/coverage.cpp
@@ -17,7 +17,7 @@
 
 using namespace std;
 
-typedef vector< shared_ptr<int> > Depth;
+typedef vector< shared_array<int> > Depth;
 typedef vector<FastASequence> Sequences;
 
 Configuration config;
@@ -53,8 +53,8 @@ bool calculateDepth(const ReadCoverage &coverage, const Sequences &contigs, Dept
     {
         int contigLength = contigs[i].Nucleotides.length();
         //depth[i].assign(contigLength, 0);
-        depth[i] = shared_ptr<int>(new int[contigLength]);
-        memset(depth[i], 0, contigLength * sizeof(int));
+        depth[i] = shared_array<int>(new int[contigLength]);
+        memset(depth[i]->get(), 0, contigLength * sizeof(int));
         //cout << "Got " << t << " " << sizeof(t) << " (aiming at " << contigLength << ")" << endl;
         for (vector<int>::const_iterator it = coverage.ReadLocations[i].begin(); it != coverage.ReadLocations[i].end(); it++)
         {
