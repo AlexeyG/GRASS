cebb5173dc380a73ee3d2846d46c0f92bca27cc4
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-04 00:20:33 +0000

    - Segfault

--- coverageUtil/coverage.cpp
+++ coverageUtil/coverage.cpp
@@ -56,7 +56,7 @@ bool calculateDepth(const ReadCoverage &coverage, const vector<FastASequence> &c
         {
             int pos = coverage.ReadLocations[i][j];
             for (int k = pos; k < pos + avgReadLength && k < contigLength; k++)
-                depth[i][k]++;
+            ;//    depth[i][k]++;
         }
     }
     return true;
