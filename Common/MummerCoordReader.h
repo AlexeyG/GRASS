9ba0bf6e87e96507dac6ff7e2d6721a4952ae1b7
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-06 13:39:41 +0000

    - Adding id conversion for MummerCoord alignments

--- Common/MummerCoordReader.h
+++ Common/MummerCoordReader.h
@@ -11,14 +11,16 @@
 #include <vector>
 #include <cstdio>
 #include <string>
+#include <map>
 #include "MummerCoord.h"
+#include "Sequence.h"
 
 using namespace std;
 
 class MummerCoordReader
 {
 public:
-    MummerCoordReader();
+    MummerCoordReader(const vector<FastASequence> &references, const vector<FastASequence> &scaffolds);
     ~MummerCoordReader();
     
 public:
@@ -33,6 +35,13 @@ private:
     FILE *fin;
     char *line;
     long long numCoords;
+    
+private:
+    map<string, int> referenceIds;
+    map<string, int> scaffoldIds;
+    
+private:
+    void createMap(map<string, int> &store, const vector<FastASequence> &seq);
 };
 
 #endif	/* _MUMMERCOORDREADER_H */
