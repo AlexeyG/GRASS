63a69494ebe62ed78f622a42aea2a959643181a7
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-07 09:44:45 +0000

    - Adding distance breakpoint count support

--- breakpointCounter/BreakpointCount.h
+++ breakpointCounter/BreakpointCount.h
@@ -17,7 +17,7 @@ using namespace std;
 class BreakpointCount
 {
 public:
-    BreakpointCount() : Joins(0), Order(0), Orientation(0), Total(0) {}
+    BreakpointCount() : Joins(0), Order(0), Orientation(0), Distance(0), Total(0) {}
     
 public:
     bool IsBreakpoint(const MummerCoord &a, const MummerCoord &b);
@@ -27,6 +27,7 @@ public:
     int Joins;
     int Order;
     int Orientation;
+    int Distance;
     int Total;
     
 private:
