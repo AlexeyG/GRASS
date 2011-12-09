471a0346bf16f79dcbeeba0a92f1a6ed81530ee2
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-09 13:28:30 +0000

    - Some overlap testing

--- scaffoldOptimizer/ScaffoldConverter.cpp
+++ scaffoldOptimizer/ScaffoldConverter.cpp
@@ -4,46 +4,71 @@
 #include "NWAligner.h"
 #include "MinMax.h"
 
+#include <iostream>
+
 using namespace std;
 
 FastASequence ScaffoldConverter::ToFasta(const DataStore &store, const Scaffold &scaffold)
 {
-	string name, sequence;
-	int count = scaffold.ContigCount();
-	int end = 0;
-	for (int i = 0; i < count; i++)
-	{
-		ScaffoldContig contig = scaffold[i];
-		FastASequence contigSeq = store[contig.Id].Sequence;
-		int contigLen = contigSeq.Nucleotides.length();
-		if (!contig.T)
-		{
-			//cout << i << " Forward: " << contig.X << " - " << end << " = " << contig.X - end << endl;
-			string spacer(max(contig.X - end, 0), 'N');
-			sequence = sequence + spacer + contigSeq.Nucleotides;
-			name = (!name.empty() ? name + "|+" : "+") + Helpers::ItoStr(contig.Id);
-			end += spacer.length() + contigLen;
-		}
-		else
-		{
-			//cout << i << " Reverse: " << contig.X << " - " << contigLen << " + 1 -" << end << " = " << contig.X - contigLen + 1 - end << endl;
-			contigSeq.ReverseCompelement();
-			string spacer(max(contig.X - contigLen + 1 - end, 0), 'N');
-			sequence = sequence + spacer + contigSeq.Nucleotides;
-			name = (!name.empty() ? name + "|-" : "-") + Helpers::ItoStr(contig.Id);
-			end += spacer.length() + contigLen;
-		}
-	}
+    string name, sequence;
+    int count = scaffold.ContigCount();
+    int solutionEnd = 0;
+    int actualEnd = 0;
+    for (int i = 0; i < count; i++)
+    {
+        ScaffoldContig contig = scaffold[i];
+        FastASequence contigSeq = store[contig.Id].Sequence;
+        int contigLen = contigSeq.Nucleotides.length();
+        if (!contig.T)
+        {
+            int solutionDistance = contig.X - solutionEnd;
+            string spacer(max(solutionDistance, 0), 'N'); // spacer sequence
+            if (solutionDistance < 0)
+            {
+                int overlapLength = -solutionDistance;
+                FastASequence seqA = FastASequence(sequence.substr(actualEnd - overlapLength, overlapLength), "prev");
+                FastASequence seqB = FastASequence(contigSeq.Nucleotides.substr(0, overlapLength), "current");
+                NWAligner aligner(seqA, seqB);
+                int score = aligner.Align();
+                cout << "Overlap: " << overlapLength << " | " << score << endl;
+            }
+            sequence = sequence + spacer + contigSeq.Nucleotides;
+            actualEnd += spacer.length() + contigLen;
+            solutionEnd += spacer.length() + contigLen;
+        }
+        else
+        {
+            contigSeq.ReverseCompelement();
+            
+            int solutionDistance = contig.X - contigLen + 1 - solutionEnd;
+            string spacer(max(solutionDistance, 0), 'N'); // spacer sequence
+            if (solutionDistance < 0)
+            {
+                int overlapLength = -solutionDistance;
+                FastASequence seqA = FastASequence(sequence.substr(actualEnd - overlapLength, overlapLength), "prev");
+                FastASequence seqB = FastASequence(contigSeq.Nucleotides.substr(0, overlapLength), "current");
+                NWAligner aligner(seqA, seqB);
+                int score = aligner.Align();
+                cout << "Overlap: " << overlapLength << " | " << score << endl;
+            }
+            
+            sequence = sequence + spacer + contigSeq.Nucleotides;
+            actualEnd += spacer.length() + contigLen;
+            solutionEnd += spacer.length() + contigLen;
+        }
+        char sign = (!contig.T ? '-' : '+');
+        name = (!name.empty() ? name + "|" + sign : sign) + Helpers::ItoStr(contig.Id);
+    }
 
-	return FastASequence(sequence, name);
+    return FastASequence(sequence, name);
 }
 
 vector<FastASequence> ScaffoldConverter::ToFasta(const DataStore &store, const vector<Scaffold> &scaffold)
 {
-	int count = scaffold.size();
-	vector<FastASequence> seq(count);
-	for (int i = 0; i < count; i++)
-		seq[i] = ToFasta(store, scaffold[i]);
+    int count = scaffold.size();
+    vector<FastASequence> seq(count);
+    for (int i = 0; i < count; i++)
+        seq[i] = ToFasta(store, scaffold[i]);
 
-	return seq;
+    return seq;
 }
