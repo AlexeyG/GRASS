81eaadde8b62a2cd65e722649c5e61bd85574fa0
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2011-12-19 16:25:07 +0000

    - Reverting old algo

--- scaffoldOptimizer/ScaffoldConverter.cpp
+++ scaffoldOptimizer/ScaffoldConverter.cpp
@@ -8,8 +8,6 @@
 
 #include <iostream>
 
-#define _OVERLAP
-
 using namespace std;
 
 vector<FastASequence> ScaffoldConverter::ToFasta(const DataStore &store, const Scaffold &scaffold, const OverlapperConfiguration &config)
