dc42f2d9e7f3b491c5b13969489531e46580ec8b
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2012-01-09 09:08:32 +0000

    - Comment change?

--- scaffoldOptimizer/ContigOverlapper.cpp
+++ scaffoldOptimizer/ContigOverlapper.cpp
@@ -40,7 +40,7 @@ int ContigOverlapper::FindBestOverlap(const std::string &left, const std::string
         // check if overlapLen is too large to align!
         if (overlapLen <= config.MaximumAlignmentLength)
         {
-            double alignmentScore = (double)GetAlignmentScore(leftSequence, rightSequence) / (double)(2 * overlapLen); // [-0.75; 1] alignment score
+            double alignmentScore = (double)GetAlignmentScore(leftSequence, rightSequence) / (double)(2 * overlapLen); // [-1.5; 1] alignment score
             double score = alignmentScore * distanceScore;
             if (score > bestScore)
             {
