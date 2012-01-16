3d1c220a93c69aefd7f172fdf784fbbb38b8e844
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2012-01-16 12:33:47 +0000

    - Adding comments (GNU GPL v3)

--- scaffoldOptimizer/OverlapperConfiguration.h
+++ scaffoldOptimizer/OverlapperConfiguration.h
@@ -1,8 +1,29 @@
-/* 
- * File:   OverlapperConfiguration.h
- * Author: alexeyg
+/*
+ * scaffoldOptimizer : solves the MIQP optimization and produces linear scaffold
+ * sequences.
+ * Copyright (C) 2011  Alexey Gritsenko
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Created on December 11, 2011, 1:34 PM
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ * 
+ * 
+ * 
+ * Email: a.gritsenko@tudelft.nl
+ * Mail: Delft University of Technology
+ *       Faculty of Electrical Engineering, Mathematics, and Computer Science
+ *       Department of Mediamatics
+ *       P.O. Box 5031
+ *       2600 GA, Delft, The Netherlands
  */
 
 #ifndef _OVERLAPPERCONFIGURATION_H
