735410174afb77cedf259c0e971b48d011dcbdc2
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2012-01-16 13:02:25 +0000

    - Adding comments (GNU GPL v3)

--- breakpointCounter/breakpoint.cpp
+++ breakpointCounter/breakpoint.cpp
@@ -1,9 +1,30 @@
-/* 
- * File:   breakpoint.cpp
- * Author: alexeyg
+/*
+ * breakpointCounter : counts scaffold breakpoint using MUMmer local alignment.
+ * Copyright (C) 2011  Alexey Gritsenko
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Created on December 5, 2011, 7:42 PM
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ * 
+ * 
+ * 
+ * Email: a.gritsenko@tudelft.nl
+ * Mail: Delft University of Technology
+ *       Faculty of Electrical Engineering, Mathematics, and Computer Science
+ *       Department of Mediamatics
+ *       P.O. Box 5031
+ *       2600 GA, Delft, The Netherlands
  */
+
 
 #include <cstdlib>
 #include <cstdio>
@@ -76,8 +97,17 @@ void output(const Coords &coords)
         cout << it->QueryID << " [" << it->QueryPosition << ", " << it->QueryPosition + it->QueryAlignmentLength << "]" << endl;
 }
 
+void banner()
+{
+    cerr << "This program comes with ABSOLUTELY NO WARRANTY; see LICENSE for details." << endl;
+    cerr << "This is free software, and you are welcome to redistribute it" << endl;
+    cerr << "under certain conditions; see LICENSE for details." << endl;
+    cerr << endl;
+}
+
 int main(int argc, char* argv[])
 {
+    banner();
     srand((unsigned int)time(NULL));
     scaffolds = auto_ptr<Sequences>(new Sequences());
     references = auto_ptr<Sequences>(new Sequences());
