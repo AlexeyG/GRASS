41f33397c5f6abfe08cd85b1623587a64ad588a1
grizenko.a@gmail.com <grizenko.a@gmail.com@72368d39-4699-a4c1-0d86-6ff09a3de2ed>
2012-01-16 12:19:34 +0000

    - Adding comments (GNU GPL v3)

--- Common/DataStoreReader.cpp
+++ Common/DataStoreReader.cpp
@@ -1,3 +1,31 @@
+/*
+ * Common : a collection of classes (re)used throughout the scaffolder implementation.
+ * Copyright (C) 2011  Alexey Gritsenko
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ * 
+ * 
+ * 
+ * Email: a.gritsenko@tudelft.nl
+ * Mail: Delft University of Technology
+ *       Faculty of Electrical Engineering, Mathematics, and Computer Science
+ *       Department of Mediamatics
+ *       P.O. Box 5031
+ *       2600 GA, Delft, The Netherlands
+ */
+
+
 #include "DataStoreReader.h"
 #include "Helpers.h"
 #include <sstream>
